5694d74e7312f5ce745bcdcd2341097ce0cc59d9
Eduardo Gonzalez Fisher <eddie@eddie.cl>
2017-04-18 11:40:19 +0300

    Add option to connect as different user

--- query-machines.sh
+++ query-machines.sh
@@ -3,9 +3,17 @@
 
 #MACHINES="almaha bashiq buraq jasmine oqab raed shihab";
 #OLD_MACHINES="almaha bashiq buraq oqab raed shihab";
+if [ "$1" == "-l" -a "a$2" != "a" ];
+then
+	u="$2@"
+	shift 2
+fi
+
+[ "a$1" == "a" ] && exit 1;
 
 for h in $MACHINES; 
 do
+	h="$u$h"
 	echo -e "\n## $h : $@ ##"
 	ssh $h $@ 2>/dev/null
 done
