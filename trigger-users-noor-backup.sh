c7356df42dd86803cdb6a6ba64f5ee2cb07835cd
Eduardo Gonzalez Fisher <eddie@eddie.cl>
2017-05-01 12:38:29 +0300

    Add file

---
+++ trigger-users-noor-backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+for username in `cut -d: -f1 /etc/passwd`; do
+	id=$(id -u $username 2>/dev/null )
+	[ -n "$id" ] && [ $id -gt 100000 ] || continue;
+
+	sudo -u $username -H /opt/noor-backup-folder.sh
+	#echo "-> $username"
+	sleep 1.3
+done
+
